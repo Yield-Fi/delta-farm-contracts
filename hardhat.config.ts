d1a09e073804243278ae639fed63b1f0f80c1e06
Michał Podsiadły <m.podsiadly@nextrope.com>
2021-09-08 19:21:55 +0200

    Pancakeswap V2 Strategy extraction + test suite

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,9 +6,9 @@ import "hardhat-deploy";
 import "solidity-coverage";
 
 import { config as dotEnvConfig } from "dotenv";
-dotEnvConfig();
-
 import { task } from "hardhat/config";
+
+dotEnvConfig();
 
 task("accounts", "Prints the list of accounts", async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
@@ -20,12 +20,28 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 
 export default {
   networks: {
+    hardhat: {
+      chainId: 31337,
+      gas: 12000000,
+      blockGasLimit: 0x1fffffffffffff,
+      allowUnlimitedContractSize: true,
+      timeout: 1800000,
+    },
     testnet: {
       url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
       accounts: [process.env.TESTNET_PRIVATE_KEY],
     },
   },
-  solidity: "0.8.3",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.3",
+      },
+      {
+        version: "0.6.6",
+      },
+    ],
+  },
   typechain: {
     outDir: "./typechain",
     target: "ethers-v5",
