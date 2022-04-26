ab39993cef73ddbebda5b12410cb8c5f9f08cc63
spaceh3ad <jan.kwiatkowski@studenci.wsksim.edu.pl>
2022-04-26 10:47:20 +0200

    removed dist

--- dist/configs/types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mainnet_1 = require("./mainnet");
-const vault = mainnet_1.mainnetConfig.vaults[0];
-const worker = vault.workers[0];
-const client = mainnet_1.mainnetConfig.clients[0];
